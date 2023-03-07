f8d69392e9663113e9a25625e8ed064aac473721
maxmurjon <maxmurjonkeldiyorov@gmail.com>
2023-03-07 17:36:42 +0500

    add: functions

---
+++ functions/parseToString.go
@@ -0,0 +1,8 @@
+package parsetostring
+
+import "strconv"
+
+func intToString(a int)(result string){
+	result=strconv.Itoa(a)
+	return
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var son int
+	fmt.Scanln(&son)
+}
