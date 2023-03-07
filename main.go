841f48cd4852dbf97c79a74db8ef5ba4b5f10fa7
maxmurjon <maxmurjonkeldiyorov@gmail.com>
2023-03-07 18:26:38 +0500

    aefesge

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"app/funcs/functions"
+	"fmt"
 )
 
 func main() {
 	var son int
 	fmt.Scanln(&son)
-	str:=functions.IntToString(son)
+	str := functions.IntToString(son)
 	functions.EvenDigitNum(str)
 }
